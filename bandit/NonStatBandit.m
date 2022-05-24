56331cb0a637b26819d02056d8dc9d2e85c2c136
Atsushi Saitoh <lynxeyed@live.jp>
2022-05-25 00:16:44 +0900

    added NonStatBandit

---
+++ bandit/NonStatBandit.m
@@ -0,0 +1,25 @@
+classdef NonStatBandit
+    properties
+        arms = 10
+        rates = []
+   end
+   methods
+        % init
+       function obj = NonStatBandit(arms)
+            if nargin == 1
+                obj.arms = arms;
+            end
+           rng('default');
+           obj.rates = rand(1, obj.arms);
+        end
+        function r = play(obj, arm)
+            rate = obj.rates(arm);
+            obj.rates = obj.rates + 0.1 .* randn(1,obj.arms); % add noise
+            if rate > rand()
+                r = 1;
+            else
+                r = 0;
+            end
+        end
+   end
+end
